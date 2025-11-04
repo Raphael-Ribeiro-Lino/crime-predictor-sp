68aba2a36060bd18a8f47f065d1527bac19da2e7
Raphael Ribeiro Lino <raphar.lino@gmail.com>
2025-11-04 16:34:26 -0300

    Atualizando template index

--- static/main.js
+++ static/main.js
@@ -1,12 +1,20 @@
 window.onload = function () {
-    //Reference the DropDownList.
-    var year = document.getElementById("year-dropdown");
+  // Referência ao select
+  var year = document.getElementById("year-dropdown");
 
-    //Loop and add the Year values to DropDownList.
-    for (var i = 2000; i <= 2050; i++) {
-        var option = document.createElement("OPTION");
-        option.innerHTML = i;
-        option.value = i;
-        year.appendChild(option);
-    }
+  // Cria a primeira opção "Selecione um ano"
+  var defaultOption = document.createElement("OPTION");
+  defaultOption.text = "Selecione um ano";
+  defaultOption.value = "";
+  defaultOption.disabled = true;
+  defaultOption.selected = true;
+  year.appendChild(defaultOption);
+
+  // Adiciona os anos de 2000 a 2050
+  for (var i = 2000; i <= 2050; i++) {
+    var option = document.createElement("OPTION");
+    option.text = i;
+    option.value = i;
+    year.appendChild(option);
+  }
 };
